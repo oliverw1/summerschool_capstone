ae02815c944d4649b5e37a1c93882d9ecdcf0601
Michiel Dhadamus <michieldhadamus@gmail.com>
2021-07-18 14:23:01 +0200

    Test fix

--- summer_capstone/scraping.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from typing import Union
-
-import openaq
-
-def query_open_aq(**kwargs) -> Union[dict, pd.DataFrame]:
-    api = openaq.OpenAQ(version="v2")
-    response = api.measurements(**kwargs)
-    return response
